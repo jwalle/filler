1dcd099ddffc0056b5e8d1196e51259730b6d1b8
Julien WALLE <jwalle@e2r5p20.42.fr>
2016-04-25 18:57:09 +0200

    display

--- bonus/srcs/bonus.c
+++ bonus/srcs/bonus.c
@@ -44,23 +44,30 @@ static void		ft_init_color(void)
 	init_pair(2048, COLOR_YELLOW, COLOR_WHITE);
 }
 
-int check_map(t_env *e)
+int check_map_bonus(t_env *e, int size[2])
 {
 	int x;
 	int y;
 	int i;
 	int len;
+	int p;
 
 	x = 0;
 	i = 0;
-	while (e->map[x])
+	while (x <= size[0])
 	{
+		p = 0;
 		y = 0;
+		if (!e->map[x])
+			return (0);
 		len = strlen(e->map[x]) * 4;
-		while (len--)
-			mvprintw(x + i, len, "_");
+			mvprintw(x + i, 0, "P");
+		while (p++ < len)
+			mvprintw(x + i, p, "_");
+		mvprintw(x + i, len, "K");
 		i++;
-		while (e->map[x][y])
+		y = 0;
+		while (y <= size[1])
 		{
 			if (e->map[x][y] == 'X')
 			{
@@ -80,14 +87,28 @@ int check_map(t_env *e)
 				mvprintw(x + i, y * 4, "| . ");
 				attroff(COLOR_PAIR(32));
 			}
+			else if (e->map[x][y] == 'o')
+			{
+				attron(COLOR_PAIR(64));
+				mvprintw(x + i, y * 4, "| o ");
+				attroff(COLOR_PAIR(64));
+			}
+			else if (e->map[x][y] == 'x')
+			{
+				attron(COLOR_PAIR(64));
+				mvprintw(x + i, y * 4, "| x ");
+				attroff(COLOR_PAIR(64));
+			}
 			mvprintw(x + i, y * 4, "|");
 			//ft_putchar_fd(e->map[x][y], 2);
 			y++;
 		}
-		mvprintw(x, y  * 4, "\n");
 		//ft_putchar_fd('\n', 2);
 		x++;
 	}
+	p  = 0;
+	while (p++ < len)
+		mvprintw(x + i, p, "_");
 	e->map_size[0] = x;
 	e->map_size[1] = y;
 	//printf("x = %i, y = %i\n", x, y);
@@ -162,7 +183,7 @@ void	init_env(t_env *e)
 int	main()
 {
 	int i;
-	//int size[2];
+	int size[2];
 	int j;
 	t_env *e;
 	//char *str;
@@ -187,12 +208,11 @@ int	main()
 		{
 			if (strstr(line, "Plateau"))
 			{
-				//get_size_bonus(line, size);
+				get_size_bonus(line, size);
 				e->map = get_map(e, line);
-				check_map(e);
-				usleep(50000);
+				check_map_bonus(e, size);
+				usleep(200000);
 				refresh();
-
 			}
 			//mvprintw(j, i, &line[i]);
 			i++;
