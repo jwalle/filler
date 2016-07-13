67252c34d10e53531a9c8b45d8698984843e2d42
Julien WALLE <jwalle@e1r6p4.42.fr>
2016-07-13 18:10:08 +0200

    shell script

--- script.sh
+++ script.sh
@@ -5,16 +5,38 @@ winp1=0
 winp2=0
 draw=0
 
-
-test_all()
+cmp_result()
 {
-	LINES=$(make test | grep "fin" | cut -d ' ' -f4)
+	if test $1 -eq $2
+	then
+		((draw++))
+	fi
+	if test $1 -gt $2
+	then
+		((winp1++))
+	fi
+	if test $1 -lt $2
+	then
+		((winp2++))
+	fi
+}
+
+get_result()
+{
+	LINE=$(make test | grep "fin" | cut -d ' ' -f4)
+	echo $LINE
+	P1=$(echo $LINE | cut -d ' ' -f1)
+	P2=$(echo $LINE | cut -d ' ' -f2)
+	cmp_result $P1 $P2
 }
 
 
 while [ $cmpt -le 5 ]
 do
-	test_all
+	get_result
 	((cmpt++))
 done
+printf "Player 1 win : %d\n" $winp1
+printf "Player 2 win : %d\n" $winp2
+printf "Draw : %d\n" $draw
 exit 0
